c68d59ee6ec28ea3903b70d8809f4194f12d12f7
VahagnCh <vahagnchavushyan@gmail.com>
2024-12-16 14:48:20 -0600

    feat: Raffle Contract- enterRaffle and pickWinner functions with a immutable variable entraceFee and its getter

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,4 +1,4 @@
-// Layout of Contract:
+// Layout of Contract: MIT
 // version
 // imports
 // errors
@@ -26,10 +26,26 @@ pragma solidity 0.8.19;
 
 /**
  * @title A sample Raffle Contract
- * @author Patrick Collins
+ * @author Vahagn Chavushyan
  * @notice This contract is for creating a sample raffle contract
  * @dev This implements the Chainlink VRF Version 2
  */
+
 contract Raffle {
-    
-}
+
+    uint256 private immutable i_entranceFee;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {}
+
+    function pickWinner() public {}
+
+
+    /** Getter Functions */
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+} 
