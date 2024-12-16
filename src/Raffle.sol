6f33f51df311b816180c9a05d99c8c6f73d4826c
VahagnCh <vahagnchavushyan@gmail.com>
2024-12-16 17:33:37 -0600

    feat: added events and state variables

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,4 +1,6 @@
-// Layout of Contract: MIT
+// SPDX-License-Identifier: MIT
+
+// Layout of Contract: 
 // version
 // imports
 // errors
@@ -19,7 +21,6 @@
 // private
 // view & pure functions
 
-// SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.19;
 
@@ -33,18 +34,36 @@ pragma solidity 0.8.19;
 
 contract Raffle {
 
+    /* Errors */
+    error Raffle__SendMoreToEnterRaffle();
+
+    /* State Variables */
+    address payable [] private s_players;
     uint256 private immutable i_entranceFee;
 
+    /* Events */
+    event RaffleEntered(address indexed player);
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    function enterRaffle() public payable {}
 
-    function pickWinner() public {}
+    /* Functions */
+    function enterRaffle() public payable {
+        //require(msg.value >= i_entranceFee, "Not enough ETH sent"); This is not gas efficient
+        if (msg.value < i_entranceFee) {
+             revert Raffle__SendMoreToEnterRaffle();
+        }
+        s_players.push(payable(msg.sender));
+        emit RaffleEntered(msg.sender);
+    }
 
+    function pickWinner() public {
+
+    }
 
     /** Getter Functions */
+
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }
